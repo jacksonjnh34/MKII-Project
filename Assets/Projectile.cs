424c38f0cfa01b6bfb80d73569db2e3bee7f93c3
Jackson Haile <jacksonjnh35@gmail.com>
2020-05-16 18:20:31 -0500

    Fixed projectile bug, projectile no longer has to be present in scene in order to be fired. Also added rudimentary collision scripting to bullet (NOT FUNCTIONAL)

---
+++ Assets/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+    	if(collision.gameObject.tag.Equals("Edge"))
+    	{
+    		//Damage code here?
+    	}
+    	else
+    	{
+    		Destroy(gameObject);
+    	}
+
+    	Destroy(gameObject);
+    	
+    }
+}
